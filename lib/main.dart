26cd97ebb0d497e22c25b2ee358bdcb2c9e87434
Jahangir Younas <74153084+Jahangir-coder@users.noreply.github.com>
2020-11-26 12:06:29 +0500

    kLargeButton font size changed

--- lib/main.dart
+++ lib/main.dart
@@ -220,7 +220,7 @@ class BMICalculator extends StatelessWidget {
     const kBottomContainerHeight = 80.0;
     const kActiveCardColour = Color(0xFF1D1E33);
     const kInactiveCardColour = Color(0xFF111328);
-    const kBottomContainerColour = Color(0xFFEB1555);
+    const kBottomContainerColour = Color(0xFFEB1554);
     const kLabelTextStyle = TextStyle(
     fontSize: 18.0;
     color: Color(0xFF8D8E98);
@@ -230,7 +230,7 @@ class BMICalculator extends StatelessWidget {
     fontWeight: FontWeight.w900,
     );
     const kLargeButtonTextStyle = TextStyle(
-    fontSize: 25.0,
+    fontSize: 20.0,
     fontWeight: FontWeight.bold,
     );
     const kTitleTextStyle = TextStyle(
