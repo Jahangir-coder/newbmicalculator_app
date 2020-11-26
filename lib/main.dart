3cbe99eff0a85ef3ddebdd9778c16f5de70da803
Jahangir Younas <74153084+Jahangir-coder@users.noreply.github.com>
2020-11-26 12:03:31 +0500

    kInactiveCardColour changed

--- lib/main.dart
+++ lib/main.dart
@@ -221,7 +221,6 @@ class BMICalculator extends StatelessWidget {
     const kActiveCardColour = Color(0xFF1D1E33);
     const kInactiveCardColour = Color(0xFF111328);
     const kBottomContainerColour = Color(0xFFEB1555);
-
     const kLabelTextStyle = TextStyle(
     fontSize: 18.0,
     color: Color(0xFF8D8E98),
